d6cde04d4b04b30768b731a3fe18d7db81241aad
Peter Neyens <peter.neyens@ext.ec.europa.eu>
2019-01-08 10:55:21 +0100

    moved ec-custom-preset to sass folder

--- ecl-builder.config.js
+++ ecl-builder.config.js
@@ -16,7 +16,7 @@ const banner = `${pkg.name} - ${
 module.exports = {
   scripts: [
     {
-      entry: path.resolve('ec-custom-preset/ec-preset-full.js'),
+      entry: path.resolve('sass/ec-custom-preset/ec-preset-full.js'),
       dest: path.resolve(outputFolder, 'js/base.js'),
       options: {
         banner,
