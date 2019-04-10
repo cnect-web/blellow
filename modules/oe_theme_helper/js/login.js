60b1b5b7ad1cce9a95636b3167168e1c5d1372a7
Unknown <unknown@example.com>
2019-04-10 18:51:03 +0200

    feat(login/register) Contribute blocks with a module. Scaffold the style and the markup. feat(theme) Adjust the header bar region

---
+++ modules/oe_theme_helper/js/login.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    ECL.initExpandables('#example-expandable-button1');
+    ECL.dropdown('#dropdown1');
+
+    ECL.initExpandables('#example-expandable-button-register');
+    ECL.dropdown('#dropdown-register');
+});
