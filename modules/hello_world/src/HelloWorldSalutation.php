0eea2f6362aa8a7125a116f704c31783ccc408fa
Unknown <unknown@example.com>
2019-04-24 14:51:00 +0200

    feat(hello) Load message from configuration

--- modules/hello_world/src/HelloWorldSalutation.php
+++ modules/hello_world/src/HelloWorldSalutation.php
@@ -12,9 +12,30 @@ class HelloWorldSalutation {
   use StringTranslationTrait;
 
   /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * HelloWorldSalutation constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   */
+  public function __construct(\Drupal\Core\Config\ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * Returns the salutation
    */
   public function getSalutation() {
+
+    $config = $this->configFactory->get('hello_world.custom_salutation');
+    $salutation = $config->get('salutation');
+    if ($salutation != "") {
+      return $salutation;
+    }
+
     $time = new \DateTime();
     if ((int) $time->format('G') >= 06 && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
