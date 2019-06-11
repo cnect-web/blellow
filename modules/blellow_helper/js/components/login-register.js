b28012ddc641cfe0c8c8fdefff79d417ead389fd
Unknown <unknown@example.com>
2019-06-11 13:52:26 +0200

    chore(modules) Kill Blellow folder. The module is moved into Packagist and is required by composer

--- modules/blellow_helper/js/components/login-register.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    ECL.initExpandables('#cnect-expandable-button-login');
-    ECL.dropdown('#cnect-dropdown-login');
-
-    ECL.initExpandables('#cnect-expandable-button-register');
-    ECL.dropdown('#cnect-dropdown-register');
-});
