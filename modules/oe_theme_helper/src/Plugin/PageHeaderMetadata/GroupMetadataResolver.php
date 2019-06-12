4d0639db916752b1437bc23a2e92ff500eea7c17
Unknown <unknown@example.com>
2019-06-12 18:49:06 +0200

    chore(page header) Scaffold group resolving

--- modules/oe_theme_helper/src/Plugin/PageHeaderMetadata/GroupMetadataResolver.php
+++ modules/oe_theme_helper/src/Plugin/PageHeaderMetadata/GroupMetadataResolver.php
@@ -10,8 +10,20 @@ namespace Drupal\oe_theme_helper\Plugin\PageHeaderMetadata;
  */
 class GroupMetadataResolver extends MetadataResolverBase {
 
+
   public function _getMetadata() {
+    $group = $this->entities->getPrimary();
+    $node = $this->entities->getRelated();
+    $visual_identity = FutGroupHelper::getVisualIdentity($group);
+    $group_operations = FutGroupHelper::getGroupOperations($group);
+    //
+    // $breadcrumb = $this->breadcrumbBuilder->build($this->requestEntityExtractor->getRouteMatch());
+
     $metadata = [
+      'group' => $group,
+      'node' => $node,
+      'visual_identity' => $visual_identity,
+      'group_operations' => $group_operations,
       '_extra' => GroupMetadataResolver::class
     ];
 
