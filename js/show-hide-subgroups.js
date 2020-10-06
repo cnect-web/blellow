dcce922ef5029c9f4f173ef0c97cbbfc76733397
Peter Neyens <peter.neyens@telenet.be>
2020-10-06 14:59:52 +0000

    improvements

--- js/show-hide-subgroups.js
+++ js/show-hide-subgroups.js
@@ -1,11 +1,13 @@
-function show_subgroups() {
-    document.getElementById('show_subgroups').style.display = 'none';
-    jQuery(".hide").show();
-    document.getElementById('hide_subgroups').style.display = 'inline-block';
-};
-
-function hide_subgroups() {
-    document.getElementById('hide_subgroups').style.display = 'none';
-    jQuery(".hide").hide();
-    document.getElementById('show_subgroups').style.display = 'inline-block';
-};
+jQuery(document).ready(function($) {
+    $("#show_subgroups").click(function() {
+        document.getElementById('show_subgroups').style.display = 'none';
+        jQuery(".hide").show();
+        document.getElementById('hide_subgroups').style.display = 'inline-block';
+    });
+    
+    $("#hide_subgroups").click(function() {
+        document.getElementById('hide_subgroups').style.display = 'none';
+        jQuery(".hide").hide();
+        document.getElementById('show_subgroups').style.display = 'inline-block';
+    });
+});
