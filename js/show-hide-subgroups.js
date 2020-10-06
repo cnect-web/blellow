b2b003e14999442e9c4266c3f1ef1d3fb613f4d0
Peter Neyens <peter.neyens@telenet.be>
2020-10-06 12:26:27 +0000

    improvement and clean up

--- js/show-hide-subgroups.js
+++ js/show-hide-subgroups.js
@@ -1,15 +1,11 @@
 function show_subgroups() {
-    if(document.getElementById('show_subgroups')) {
-        document.getElementById('show_subgroups').style.display = 'none';
-        jQuery(".hide").show();
-        document.getElementById('hide_subgroups').style.display = 'block';
-    }
+    document.getElementById('show_subgroups').style.display = 'none';
+    jQuery(".hide").show();
+    document.getElementById('hide_subgroups').style.display = 'inline-block';
 };
 
 function hide_subgroups() {
-    if(document.getElementById('show_subgroups')) {
-        document.getElementById('hide_subgroups').style.display = 'none';
-        jQuery(".hide").hide();
-        document.getElementById('show_subgroups').style.display = 'block';
-    }
+    document.getElementById('hide_subgroups').style.display = 'none';
+    jQuery(".hide").hide();
+    document.getElementById('show_subgroups').style.display = 'inline-block';
 };
